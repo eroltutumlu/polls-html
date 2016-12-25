1ec489d09d58defb71c439eb4e515520b0a690c0
Erol <eroltutumlu@gmail.com>
2016-12-25 23:45:14 +0100

    Removed inline css and js

---
+++ resources/js/home-charts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  var options = {
+	  chart:{
+		  renderTo:'poll',
+		  type:'bar'
+	  },
+	  title:{
+		  text:'Which programming language do you prefer?'
+	  },
+	  xAxis:{
+		  categories:[
+			  'Java',
+			  'C#',
+			  'Python'
+		  ]
+	  },
+	  yAxis:{
+		  title:{
+			  text:'According to the users'
+		  }
+	  },
+	  series:[
+		  {
+			  name:'Users',
+			  data:[5,9,4]
+		  }
+	  ]
+  };
+
+  var barChart = new Highcharts.chart(options); 
+});
